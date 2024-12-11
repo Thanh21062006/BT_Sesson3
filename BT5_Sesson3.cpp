7888bcaaf2c17ec5901056b425373764d14d9a89
Thanh21062006 <caotatthanh2006@gmail.com>
2024-12-11 23:49:46 +0700

    Add files via upload

---
+++ BT5_Sesson3.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+	printf("%70s\n","DANH SACH SINH VIEN");
+		printf("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+	printf("| %-3s | %-15s | %-4s | %-15s | %-24s |\n", "STT", "Ho va ten", "Tuoi", "So dien thoai", "email");
+		printf("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+	printf("| %-3s | %-15s | %-4s | %-15s | %-25s |\n", "1", "Nguyen Van A", "20", "0904488481", "anv@rikkeiacadamy.com");
+		printf("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+	printf("| %-3s | %-15s | %-4s | %-15s | %-25s |\n", "2", "Nguyen Van B", "21", "0904488482", "bnv@rikkeiacadamy.com");
+		printf("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+	printf("| %-3s | %-15s | %-4s | %-15s | %-25s |\n", "3", "Nguyen Van C", "18", "0904488483", "cnv@rikkeiacadamy.com");
+		printf("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+	printf("| %-3s | %-15s | %-4s | %-15s | %-25s |\n", "4", "Nguyen Van D", "19", "0904488484", "dnv@rikkeiacadamy.com");
+		printf("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+	printf("| %-3s | %-15s | %-4s | %-15s | %-25s |\n", "5", "Nguyen Van E", "22", "0904488485", "env@rikkeiacadamy.com");
+		printf("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+	printf("| %-3s | %-15s | %-4s | %-15s | %-25s |\n", "6", "Nguyen Van F", "21", "0904488486", "fnv@rikkeiacadamy.com");
+		printf("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+	printf("| %-3s | %-15s | %-4s | %-15s | %-25s |\n", "7", "Nguyen Van G", "23", "0904488487", "gnv@rikkeiacadamy.com");
+		printf("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+	printf("| %-3s | %-15s | %-4s | %-15s | %-25s |\n", "8", "Nguyen Van H", "19", "0904488488", "hnv@rikkeiacadamy.com");
+		printf("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+	printf("| %-3s | %-15s | %-4s | %-15s | %-25s |\n", "9", "Nguyen Van I", "18", "0904488489", "inv@rikkeiacadamy.com");
+		printf("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+	printf("| %-3s | %-15s | %-4s | %-15s | %-25s |\n", "10", "Nguyen Van K", "21", "0904488480", "knv@rikkeiacadamy.com");
+		printf("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+	return 0;
+	
+}
